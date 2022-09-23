db64c780c635aef5e68c8050785541bbdbc9a5ab
Alfilister <castillo.c.jm@gmail.com>
2022-09-23 11:40:46 -0500

    fs used again

--- src/db.js
+++ src/db.js
@@ -11,7 +11,7 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env
 //     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 //   }
 // )
-// const basename = path.basename(__filename)
+const basename = path.basename(__filename)
 
 const sequelize =
   process.env.NODE_ENV === "production"
@@ -47,17 +47,17 @@ const sequelize =
 const modelDefiners = []
 
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
-// fs.readdirSync(path.join(__dirname, "/models"))
-//   .filter(
-//     (file) =>
-//       file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-//   )
-//   .forEach((file) => {
-//     modelDefiners.push(require(path.join(__dirname, "/models", file)))
-//   })
+fs.readdirSync(path.join(__dirname, "/models"))
+  .filter(
+    (file) =>
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+  )
+  .forEach((file) => {
+    modelDefiners.push(require(path.join(__dirname, "/models", file)))
+  })
 
-modelDefiners.push(require("./models/Genre"))
-modelDefiners.push(require("./models/Videogame"))
+// modelDefiners.push(require("./models/Genre"))
+// modelDefiners.push(require("./models/Videogame"))
 
 // Injectamos la conexion (sequelize) a todos los modelos
 modelDefiners.forEach((model) => model(sequelize))

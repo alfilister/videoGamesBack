727647fe4f695a9a1a46e13fc78800a4dbfed14d
Alfilister <castillo.c.jm@gmail.com>
2022-09-23 12:01:35 -0500

    Local issue fixed with db_name

--- index.js
+++ index.js
@@ -19,10 +19,11 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js")
 const { conn } = require("./src/db.js")
+const port = process.env.PORT || 3001
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log(`The server is excecuting at port ${process.env.PORT}`) // eslint-disable-line no-console
+conn.sync({ force: false }).then(() => {
+  server.listen(port, () => {
+    console.log(`The server is excecuting at port ${port}`) // eslint-disable-line no-console
   })
 })
